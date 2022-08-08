1c15ac0d733a67ad799b444d5de956da15fb305e
cperesso <110819326+cperesso@users.noreply.github.com>
2022-08-08 11:41:10 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html language="en">
+    <head>
+        <title> My first PHP page </title>
+</head>
+<body> 
+    <h1> Here comes some PHP:</h1>
+    <?php
+    echo "Hello, World!";
+    ?>
+</body>
+</html>
